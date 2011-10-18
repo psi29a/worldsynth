71daa4569232238d329081e90928ad2d865b894a
Bret Curtis <psi29a@gmail.com>
2011-10-18 15:10:05 +0200

    corrected astar algo with cost analysis and heavily wip riverlake code

--- library/riversAndLakes.py
+++ library/riversAndLakes.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 import math, random, sys
+from time import time
 from numpy import *
 from progressbar import ProgressBar, Percentage, ETA
 from constants import *
-from astar import AStar
+from aStar import *
 
 class Bunch(dict): # be careful to delete after use, circular references are bad
     def __init__(self,**kw):
@@ -21,7 +22,7 @@ class Rivers():
         self.riverList = []
 
     def run(self):
-        widgets = ['Generating river sources: ', Percentage(), ' ', ETA() ]
+        widgets = ['Generating rivers: ', Percentage(), ' ', ETA() ]
         pbar = ProgressBar(widgets=widgets, maxval=self.worldH*self.worldH)
         # iterate through map and mark river sources
         for x in range(1,self.worldW-1):
@@ -34,20 +35,27 @@ class Rivers():
                 # chance of river if rainfall is high and elevation around base of mountains
 
                 # no rivers in 10x10 areas
-                if self.isRiverInRange(20,x,y):
+                if self.isRiverInRange(int(math.log(self.worldW,2)*5),x,y):
                     continue
 
-                if random.random() <= 0.01:
+                if random.random() <= 0.05:
                     # chance of river source
                     river = Bunch()
                     river.x = x
                     river.y = y
+                    seaPath = self.findClosestSea(river)
+                    if seaPath: # found sea, lets find a path to item_id
+                        newPath = self.makePath(river, seaPath )
+                    else:
+                        print "Found lake."                                     
                     self.riverList.append(river)
                     del river
                     self.riverMap[x,y] = 0.1    # source is always a small in beginning
                     print x,y
             pbar.update(x+y)
         pbar.finish()
+
+        return
 
         #print len(self.riverList),maxSteps
 
@@ -60,42 +68,121 @@ class Rivers():
             while True:
 
                 newPath = {}
-                newPath = self.findQuickPath(river)
-                
+                print "Finding path for river: ", river
+                seaPath = self.findClosestSea(river)
+
+#                newPath = self.findQuickPath(river)
+#                
+                if seaPath: # found sea, lets find a path to item_id
+                    newPath = self.makePath(river, seaPath )
+                    
                 if newPath:
                     river.x = newPath['x']
                     river.y = newPath['y']
+                else:
+                    break
 
                 # did we get to the sea?
                 if self.heightmap[river.x,river.y] <= WGEN_SEA_LEVEL:
                     print "A river made it out to sea."
                     break
 
-                # check if new river direction is an existing river
-                if newPath and self.riverMap[river.x,river.y]:
-                    print "A river found another river."
-                    # follow route but strengthen the river
-                    break #TODO
+#                # check if new river direction is an existing river
+#                if newPath and self.riverMap[river.x,river.y]:
+#                    print "A river found another river."
+#                    # follow route but strengthen the river
+#                    break #TODO
 
-                if not newPath:
-                    print "Trying to find another route to sea..."
-                    newPath = self.findClosestSea(river)
-                    if newPath:
-                        river.x = newPath['x']
-                        river.y = newPath['y']
-                    else:
-                        "River turns into a lake, begin flooding area..."
-                        break
+#                if not newPath:
+#                    print "Trying to find another route to sea..."
+#                    newPath = self.findClosestSea(river)
+#                    if newPath:
+#                        river.x = newPath['x']
+#                        river.y = newPath['y']
+#                    else:
+#                        "River turns into a lake, begin flooding area..."
+#                        break
 
                 self.riverMap[river.x,river.y] = 0.1
-
+                break
 
             counter += 1
             pbar.update(counter)
 
         pbar.finish()
 
-    def findClosestSea(self, river, depth=20): # 10 = 10x10 area is -10 to 10
+
+    def findClosestSea(self, river):
+        '''Try to find the closest sea by avoiding watersheds, the returned
+        value should be a point in sea itself '''
+        
+        searchRange = 1
+        while searchRange < self.worldW:
+        
+            for x in range(-searchRange,searchRange+1):
+                for y in range(-searchRange,searchRange+1):
+                    # verify that we do not go over the edge
+                    if river.x+x >= 0 and river.y+y >= 0 and \
+                    river.x+x < self.worldW and river.y+y < self.worldH:                
+                        elevation = self.heightmap[river.x+x,river.y+y]
+                        #if elevation > BIOME_ELEVATION_MOUNTAIN:
+                        #    break # we hit the watershed, do not continue
+                            
+                        if elevation < WGEN_SEA_LEVEL:
+                            # possible sea, lets check it out...
+                            # is the spot surrounded by sea?
+                            isSea = True
+                            for i in range(-1,2):
+                                for j in range(-1,2):
+                                    if river.x+x+i >= 0 and river.y+y+j >= 0 and \
+                                    river.x+x+i < self.worldW and river.y+y+j < self.worldH:                                
+                                        if self.heightmap[river.x+x+i,river.y+y+j] > WGEN_SEA_LEVEL:
+                                            isSea = False
+                            
+                            if isSea:
+                                print "Found sea at: ", river.x+x,river.y+y
+                                return {'x':river.x+x, 'y':river.y+y, 'searchRange':searchRange}
+        
+            searchRange *= 2 # double are search area
+
+        print "Could not find sea."
+        return {}        
+
+    def in_circle_sqrt(center_x, center_y, radius, x, y):
+        dist = math.sqrt((center_x - x) ** 2 + (center_y - y) ** 2)
+        return dist <= radius
+        
+    def in_circle(center_x, center_y, radius, x, y):
+        square_dist = (center_x - x) ** 2 + (center_y - y) ** 2
+        return square_dist <= radius ** 2        
+
+    def makePath(self, river, newPath):
+        heightmap = self.heightmap.reshape(self.worldW * self.worldH)
+        
+        pathFinder = AStar(SQ_MapHandler(heightmap,self.worldW,self.worldH))
+        start = SQ_Location(river.x,river.y)
+        end = SQ_Location(newPath['x'],newPath['y'])
+        
+        s = time()
+        p = pathFinder.findPath(start,end)
+        e = time()
+        
+        if not p:
+            print "No path found! It took %f seconds." % (e-s)
+            return {}
+        else:
+            print "Found path in %d moves and %f seconds." % (len(p.nodes),(e-s))
+            for n in p.nodes:
+                #self.pathlines.append((n.location.x*16+8,n.location.y*16+8))
+                #print river.x, river.y, n.location.x, n.location.y, newPath['x'], newPath['y']
+                if self.riverMap[n.location.x,n.location.y] > 0.0:
+                    print "River found another river"
+                    break #TODO: add more river strength
+                self.riverMap[n.location.x,n.location.y] = 0.1
+            return {'x':newPath['x'], 'y':newPath['y']}
+    
+
+    def pathFindToLocation(self, river, depth=20): # 10 = 10x10 area is -10 to 10
         temp = delta = 0.0
         currentElevation = self.heightmap[river.x, river.y]
         newPath = {}
@@ -112,24 +199,44 @@ class Rivers():
                     
         if newPath:
             print "Found a possible lowest point..."
-            pathFinder = AStar(self.heightmap, (river.x, river.y), (newPath['x'],newPath['y']), EUCLIDEAN)
-            for i in pathFinder.step():
-                pass
+#            pathFinder = AStar(self.heightmap, (river.x, river.y), (newPath['x'],newPath['y']), EUCLIDEAN)
+#            for i in pathFinder.step():
+#                pass
             
-            if pathFinder.path:
-                print "Found path: ", pathFinder.path
-            else:
-                print "Could not find a new path."
+            # flatten heightmap
+            heightmap = self.heightmap.reshape(self.worldW * self.worldH)
+            
+            pathFinder = AStar(SQ_MapHandler(heightmap,self.worldW,self.worldH))
+            start = SQ_Location(river.x,river.y)
+            end = SQ_Location(newPath['x'],newPath['y'])
+            s = time()
+            p = pathFinder.findPath(start,end)
+            e = time()
+            
+            if not p:
+                print "No path found!"
                 return {}
+            else:
+                print "Found path in %d moves and %f seconds." % (len(p.nodes),(e-s))
+                for n in p.nodes:
+                    #self.pathlines.append((n.location.x*16+8,n.location.y*16+8))
+                    #print river.x, river.y, n.location.x, n.location.y, newPath['x'], newPath['y']
+                    self.riverMap[n.location.x,n.location.y] = 0.1
+
+#            if pathFinder.path:
+#                print "Found path: ", pathFinder.path
+#            else:
+#                print "Could not find a new path."
+#                return {}
             
-            for path in pathFinder.path:
-                self.riverMap[path[0],path[1]] = 0.1
-                if self.heightmap[path[0],path[1]] > currentElevation:
-                   self.heightmap[path[0],path[1]] = currentElevation
-                else:
-                    currentElevation = self.heightmap[path[0],path[1]]
-            
-            return newPath
+#            for path in pathFinder.path:
+#                self.riverMap[path[0],path[1]] = 0.1
+#                if self.heightmap[path[0],path[1]] > currentElevation:
+#                   self.heightmap[path[0],path[1]] = currentElevation
+#                else:
+#                    currentElevation = self.heightmap[path[0],path[1]]
+#            
+#            return newPath
 
     def findQuickPath(self, river):
         # Water flows based on cost, seeking the higest elevation difference
@@ -253,22 +360,22 @@ class Rivers():
         # no river found
         return False
 
-    def makePath(self,currentX,currentY,river):
-        currentElevation = self.heightmap[currentX,currentY]
-        stepX = 1
-        stepY = 1
-        if river.x - currentX < 0:
-            stepX = -1
-        if river.y - currentY < 0:
-            stepY = -1
-        
-        while currentX != river.x and currentY != river.y:
-            currentX += stepX
-            self.heightmap[currentX,currentY] = currentElevation
-            self.riverMap[currentX,currentY] = 0.1
-            currentY += stepY
-            self.heightmap[currentX,currentY] = currentElevation
-            self.riverMap[currentX,currentY] = 0.1
+#    def makePath(self,currentX,currentY,river):
+#        currentElevation = self.heightmap[currentX,currentY]
+#        stepX = 1
+#        stepY = 1
+#        if river.x - currentX < 0:
+#            stepX = -1
+#        if river.y - currentY < 0:
+#            stepY = -1
+#        
+#        while currentX != river.x and currentY != river.y:
+#            currentX += stepX
+#            self.heightmap[currentX,currentY] = currentElevation
+#            self.riverMap[currentX,currentY] = 0.1
+#            currentY += stepY
+#            self.heightmap[currentX,currentY] = currentElevation
+#            self.riverMap[currentX,currentY] = 0.1
                                 
 if __name__ == '__main__':
     print "hello!"
